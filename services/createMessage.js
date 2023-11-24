cb4f249f7b8e77b802b4c2c4d5b9a2d76fc7ea2c
NewHope1396 <rimondtt@gmail.com>
2023-11-24 16:18:03 +0200

    consultaion message added, order message edited

--- services/createMessage.js
+++ services/createMessage.js
@@ -1,15 +1,26 @@
 const noData = "Не вказано";
 
-const createMessage = ({ name, tel, brand, model, comment }) => {
-  return `
+const createMessage = ({ type, name, tel, brand, model, area, comment }) => {
+  const orderMessage = `
   Нове Замовлення!!!
   
   Ім'я: ${name || noData}
   Телефон: ${tel}
   Марка: ${brand?.label || noData}
   Модель: ${model?.label || noData}
+  Область: ${area || noData}
   Коментар: ${comment || noData}
   `;
+
+  const consultationMessage = `
+  Консультація!!!
+  
+  Ім'я: ${name || noData}
+  Телефон: ${tel}
+  Коментар: ${comment || noData}
+  `;
+
+  return type === "order" ? orderMessage : consultationMessage;
 };
 
 module.exports = createMessage;
